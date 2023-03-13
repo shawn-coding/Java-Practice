c72e49f24927732fa818a040a2db01880047e966
shawn-coding <shawncode484@gmail.com>
2023-03-12 20:33:37 -0400

    Add method exploring String Immutability.

--- Basics/Strings/StringBasics.java
+++ Basics/Strings/StringBasics.java
@@ -9,6 +9,8 @@ class StringBasics {
         String s1 = new String("Hello");
         String s2 = new String("Hello");
         //check if the references are the same
+        System.out.println("/----String Equality----/");
+        System.out.println("Two strings, first says: " + s1 + ", second says: " + s2);
         System.out.println("Does s1 == s2?: " + (s1 == s2));
         //check if the content is the same
         System.out.println("Does s1.equals(s2)?: " + s1.equals(s2));
@@ -26,8 +28,24 @@ class StringBasics {
         * always encapsulate it in parentheses
         */
     }
+
+    public static void stringImmutability(){
+        //Practice for Strings being "Inherently Immutable,", or that
+        //their internal state cannot be changed once they've been created.
+        System.out.println("/----String Immutability----/");
+        String name = "Helga";
+        System.out.println("We have a string, it is called: " + name);
+        System.out.println("We want to change it, but can't directly.");
+        System.out.print("We can make a new variable: ");
+        String otherName = name.replace("ga","en");
+        System.out.println(otherName);
+        System.out.println("we can replace it directly in output: " + name.replace("ga","en"));
+        System.out.println("But we can't change the actual content of the string itself.");
+
+    }
     public static void main(String[] args){
         stringEquality();
+        stringImmutability();
     }
 
 }
